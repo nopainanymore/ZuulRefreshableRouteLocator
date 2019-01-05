b1799038fcc3507735b07ad493ced8385d491a09
李旺 <36151000+Lee875083146@users.noreply.github.com>
2019-01-06 00:14:00 +0800

    20190106
    Filter

--- apigateway/src/main/java/com/cnsuning/promes/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/com/cnsuning/promes/apigateway/ApigatewayApplication.java
@@ -1,8 +1,10 @@
 package com.cnsuning.promes.apigateway;
 
+import com.cnsuning.promes.apigateway.filters.PreTestFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -12,5 +14,9 @@ public class ApigatewayApplication {
 		SpringApplication.run(ApigatewayApplication.class, args);
 	}
 
+	@Bean
+	public PreTestFilter preTestFilter() {
+		return new PreTestFilter();
+	}
 }
 
---
+++ apigateway/src/main/java/com/cnsuning/promes/apigateway/filters/PreTestFilter.java
@@ -0,0 +1,40 @@
+package com.cnsuning.promes.apigateway.filters;
+
+import com.google.gson.Gson;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author lw875
+ * @create 2019/1/5  23:14
+ **/
+public class PreTestFilter extends ZuulFilter {
+
+    private static final Logger log = LoggerFactory.getLogger(PreTestFilter.class);
+
+    private static final Gson gson = new Gson();
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        log.info("this is pre filter");
+        return null;
+    }
+}
