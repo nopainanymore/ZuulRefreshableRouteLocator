68010e542743a397d0d836e7bcbeb517540240d3
Lee875083146 <vbof53@163.com>
2019-07-10 00:02:15 +0800

    fix mapper can't find

--- apigateway/src/main/java/com/nopainanymore/apigateway/ApiGatewayApplication.java
+++ apigateway/src/main/java/com/nopainanymore/apigateway/ApiGatewayApplication.java
@@ -2,6 +2,7 @@ package com.nopainanymore.apigateway;
 
 
 import com.nopainanymore.apigateway.filters.PreTestFilter;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
+@MapperScan("com.nopainanymore.apigateway.dao")
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
